543646e480b8fc4aab6da67ef81f75bf87684c01
Vini <vinitran124@gmail.com>
2021-12-04 13:58:51 +0700

    Vinh

--- src/Sound/Sound.java
+++ src/Sound/Sound.java
@@ -1,6 +1,5 @@
 package Sound;
 
-
 public class Sound {
     public static final LoadSound sound_loop = new LoadSound("Sound/Loop.wav");
     public static final LoadSound sound_gameOver = new LoadSound("Sound/GameOver.wav");
