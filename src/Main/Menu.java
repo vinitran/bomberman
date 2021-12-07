92c870aace49ea210413cf67a219023a290410ec
Vini <vinitran124@gmail.com>
2021-12-07 21:00:08 +0700

    vinh upgrade

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -9,6 +9,7 @@ public class Menu implements MouseListener{
     GamePanel gamePanel;
     private MouseHandler mouseHandler;
     private double scale;
+    private int index = 0;
     public Menu(GamePanel gamePanel, MouseHandler mouseHandler) {
         this.gamePanel = gamePanel;
         this.mouseHandler = mouseHandler;
@@ -21,23 +22,35 @@ public class Menu implements MouseListener{
                 g2.drawImage(Image.start_game,600,250,144,24,null);
                 g2.drawImage(Image.continueGame,580,280,192,30,null);
                 g2.drawImage(Image.howToPlay,580,310,192,33,null);
-                gamePanel.gameState = gamePanel.playState ;
+                index = 1;
             } else if ("Coutinue".equals(mouseHandler.direction)) {
                 g2.drawImage(Image.start_game,580,250,192,32,null);
                 g2.drawImage(Image.continueGame,600,280,144,24,null);
                 g2.drawImage(Image.howToPlay,580,310,192,33,null);
-                gamePanel.gameState = gamePanel.playState ;
+                index = 2;
+
             } else if ("HowToPlay".equals(mouseHandler.direction)) {
                 g2.drawImage(Image.start_game,580,250,192,32,null);
                 g2.drawImage(Image.continueGame,580,280,192,30,null);
                 g2.drawImage(Image.howToPlay,600,310,144,24,null);
+                index = 3;
             }
         } else {
+            switch (index) {
+                case 1:
+                    gamePanel.gameState = gamePanel.playState;
+                    break;
+                case 2:
+                    gamePanel.gameState = gamePanel.playState;
+                    break;
+
+            }
             g2.drawImage(Image.start_game,580,250,192,32,null);
             g2.drawImage(Image.continueGame,580,280,192,30,null);
             g2.drawImage(Image.howToPlay,580,310,192,33,null);
-        }
+            index = 0;
 
+        }
     }
 
     @Override
