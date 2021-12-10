e3ed59ab85af3352f4d20706b73ad48b43d7566f
= <=>
2021-12-10 18:41:30 +0700

    pull

--- src/Sound/Sound.java
+++ src/Sound/Sound.java
@@ -1,9 +1,9 @@
 package Sound;
 
 public class Sound {
-    public static final LoadSound sound_loop = new LoadSound("Sound/Loop.wav");
-    public static final LoadSound sound_gameOver = new LoadSound("Sound/GameOver.wav");
-    public static final LoadSound sound_Bomberman = new LoadSound("Sound/Bomberman.wav");
-    public static final LoadSound sound_Bom = new LoadSound("Sound/BOM_11_L.wav");
-    public static final LoadSound sound_makeBom = new LoadSound("Sound/Link bonuslife.wav");
+    public static final LoadSound sound_loop = new LoadSound("sound/Loop.wav");
+    public static final LoadSound sound_gameOver = new LoadSound("sound/GameOver.wav");
+    public static final LoadSound sound_Bomberman = new LoadSound("sound/Bomberman.wav");
+    public static final LoadSound sound_Bom = new LoadSound("sound/BOM_11_L.wav");
+    public static final LoadSound sound_makeBom = new LoadSound("sound/Link bonuslife.wav");
 }
